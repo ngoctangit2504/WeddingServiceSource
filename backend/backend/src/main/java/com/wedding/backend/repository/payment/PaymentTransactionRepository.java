cdc411093c543eb16635d15eb7bcf9f4170c34da
LenioNguyen <trongkhoin315@gmail.com>
2024-08-17 23:51:59 +0700

    Fix Entity

--- backend/backend/src/main/java/com/wedding/backend/repository/payment/PaymentTransactionRepository.java
+++ backend/backend/src/main/java/com/wedding/backend/repository/payment/PaymentTransactionRepository.java
@@ -1,6 +1,5 @@
 package com.wedding.backend.repository.payment;
 
-import com.wedding.backend.entity.PaymentTransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
