cd157b3991c0219ef9c719f3d200314dfa4666c9
dungptde150381 <dungptde150381@fpt.edu.vn>
2024-08-18 21:56:53 +0700

    fix merchant enity

--- backend/backend/src/main/java/com/wedding/backend/repository/payment/MerchantRepository.java
+++ backend/backend/src/main/java/com/wedding/backend/repository/payment/MerchantRepository.java
@@ -1,6 +1,5 @@
 package com.wedding.backend.repository.payment;
 
-import com.wedding.backend.entity.MerchantEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
